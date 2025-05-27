61b8bae5576fb9b36d05e3930493d9626c191825
GuiilhermeLsantos <guilherme1907lima@gmail.com>
2025-05-26 22:42:19 -0300

    Projeto Starbuckst

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+let circulo = document.querySelector(".circulo")
+
+function trocarCor(cor){
+    circulo.style.background = cor
+}
+
+let imagem = document.querySelector(".starbucks")
+
+function trocaImagem(imgs){
+    imagem.src = imgs
+}
